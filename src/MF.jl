0b761c3712e7e1d89c5aef13d2cf95e94447451d
lepisma <abhinav.tushar.vs@gmail.com>
2014-06-15 00:24:03 +0530

    Add mean_at function for every mf
    mean_at function returns the mean value of the clipped top after clipping
    using a firing strength. Will be used by defuzz

--- src/MF.jl
+++ src/MF.jl
@@ -1,12 +1,12 @@
 module MF
 
-##########################################
+#-----------------------------------------
 # Module defining the membership functions
-##########################################
+#-----------------------------------------
 
 export TriangularMF, GaussianMF, BellMF, TrapezoidalMF
 
-##########################################
+#-----------------------------------------
 
 type TriangularMF
 	# Triangular membership function type
@@ -16,6 +16,7 @@ type TriangularMF
 	r_vertex
 
 	eval::Function
+	mean_at::Function
 
 	function TriangularMF(l_vertex, center, r_vertex)
 		if l_vertex <= center <= r_vertex
@@ -24,13 +25,21 @@ type TriangularMF
 				maximum([minimum([((x - l_vertex) / (center - l_vertex)), ((r_vertex - x) / (r_vertex - center))]), 0])
 			end
 			
-			new(l_vertex, center, r_vertex, eval)
+			function mean_at(firing_strength)
+				p1 = (center - l_vertex) * firing_strength + l_vertex
+				p2 = (center - r_vertex) * firing_strength + r_vertex
+				(p1 + p2) / 2
+			end
+			
+			new(l_vertex, center, r_vertex, eval, mean_at)
 		else
 			error("invalid vertices")
 		end
 	end
 	
 end
+
+#-----------------------------------------
 
 type GaussianMF
 	# Gaussian membership function
@@ -39,6 +48,7 @@ type GaussianMF
 	sigma
 	
 	eval::Function
+	mean_at::Function
 	
 	function GaussianMF(center, sigma)
 	
@@ -46,9 +56,17 @@ type GaussianMF
 			e ^ ( - 0.5 * ((x - center) / sigma) ^ 2)
 		end
 		
-		new(center, sigma, eval)
+		function mean_at(firing_strength)
+			p1 = center + sigma * sqrt(-2 * log(firing_strength))
+			p2 = 2 * center - p1
+			(p1 + p2) / 2
+		end
+		
+		new(center, sigma, eval, mean_at)
 	end
 end
+
+#-----------------------------------------
 
 type BellMF
 	# Generalised Bell membership function
@@ -58,16 +76,25 @@ type BellMF
 	c
 	
 	eval::Function
+	mean_at::Function
 	
 	function BellMF(a, b, c)
 	
 		function eval(x)
-			1 / (1 + ((x - c) / a) ^ (2 * b))
+			1 / (1 + abs((x - c) / a) ^ (2 * b))
 		end
 		
-		new(a, b, c, eval)
+		function mean_at(firing_strength)
+			p1 = center + a * (((1 / firing_strength) - 1) ^ (-2 * b))
+			p2 = 2 * center - p1
+			(p1 + p2) / 2
+		end
+		
+		new(a, b, c, eval, mean_at)
 	end
 end
+
+#-----------------------------------------
 
 type TrapezoidalMF
 	# Trapezoidal membership function
@@ -78,6 +105,7 @@ type TrapezoidalMF
 	r_bottom_vertex
 	
 	eval::Function
+	mean_at::Function
 	
 	function TrapezoidalMF(l_bottom_vertex, l_top_vertex, r_top_vertex, r_bottom_vertex)
 		if l_bottom_vertex <= l_top_vertex <= r_top_vertex <= r_bottom_vertex
@@ -86,7 +114,13 @@ type TrapezoidalMF
 				maximum([minimum([((x - l_bottom_vertex) / (l_top_vertex - l_bottom_vertex)), 1, ((r_bottom_vertex - x) / (r_bottom_vertex - r_top_vertex))]), 0])
 			end
 			
-			new(l_bottom_vertex, l_top_vertex, r_top_vertex, r_bottom_vertex, eval)
+			function mean_at(firing_strength)
+				p1 = (l_top_vertex - l_bottom_vertex) * firing_strength + l_bottom_vertex
+				p2 = (r_top_vertex - r_bottom_vertex) * firing_strength + r_bottom_vertex
+				(p1 + p2) / 2
+			end
+			
+			new(l_bottom_vertex, l_top_vertex, r_top_vertex, r_bottom_vertex, eval, mean_at)
 		else
 			error("invalid vertices")
 		end
