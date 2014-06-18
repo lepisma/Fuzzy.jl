7cd9dae08e4af5131d576f37b5275cd47363f6ac
lepisma <abhinav.tushar.vs@gmail.com>
2014-06-19 00:09:03 +0530

    Add sugeno type

--- src/FIS.jl
+++ src/FIS.jl
@@ -7,10 +7,11 @@ type Rule
 	# Properties
 	# ----------
 	# `input_mf_names` is a Vector of ASCIIString (in order of inputs) which contains name of membership functions
-	# `output_mf_name` is an ASCIIString containing the connected output membership function name with `input_mf_names`
+	# `output_mf` is either an ASCIIString containing the connected output membership function name with `input_mf_names`
+	# 		or is a Vector of Float64 containing consequence parameters in case of Sugeno fis
 	
 	input_mf_names::Vector{ASCIIString}
-	output_mf_name::ASCIIString
+	output_mf::Union(Vector{Float64}, ASCIIString)
 
 end
 
@@ -30,4 +31,19 @@ type FISMamdani
 	output_mfs_dict::Dict{Any, Any}
 	rules::Vector{Rule}
 
+end
+
+type FISSugeno
+	# Fuzzy Inference System of Mamdani type
+	#	
+	# Properties
+	# ----------
+	# `input_mfs_dicts` is a Vector of Dict (one for each input) which contains membership function
+	# 		associated with its name
+	# 		e.g. "small" => TriangularMF(1, 2, 3)
+	# `rules` is a Vector of Rule (of sugeno type) that form the inference system
+	
+	input_mfs_dicts::Vector{Dict{Any, Any}}
+	rules::Vector{Rule}
+	
 end
